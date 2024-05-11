647451c09eef19124c4c765128392685322b1e1d
samirkt <sktownsley@ucdavis.edu>
2024-05-11 14:12:50 -0700

    [Infra] add unit testing and pkg framework

--- src/config.py
+++ src/config.py
@@ -2,4 +2,6 @@ import midi_backend
 from modes import learner, practice
 
 MIDI_BACKEND_CLS = midi_backend.TestMidiBackend
-START_MODE = learner.ChordGameFSM #practice.ChordViz
+#MIDI_BACKEND_CLS = midi_backend.MidiBackend
+START_MODE = learner.ChordGameFSM
+#START_MODE = practice.ChordViz
