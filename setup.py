6ca13e647c47eef88c325e9f67438c1c20c5b673
samirkt <samir@advanced.farm>
2024-05-12 17:28:45 -0700

    [Feature] add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 # setup.py
 from setuptools import setup, find_packages
 
+
+requirements = [
+    "pygame",
+]
+
 setup(
     name="piano_reader",
     version="0.1",
     packages=find_packages(),
-    test_suite="tests"
+    test_suite="tests",
+    install_requires=requirements,
 )
